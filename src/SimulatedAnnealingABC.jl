c4371abd2ac13c91d434460d830787c563c44988
scheidan <andreas.scheidegger@eawag.ch>
2023-07-04 15:19:06 +0200

    rename variables

--- src/SimulatedAnnealingABC.jl
+++ src/SimulatedAnnealingABC.jl
@@ -5,49 +5,36 @@ using LinearAlgebra
 using Random
 
 using UnPack: @unpack
-using Roots: find_zero
 using StatsBase: mean, cov, ecdf, sample, weights
 using Distributions: Distribution, pdf, MvNormal
+import Roots
 import ProgressMeter
 
 export sabc
-
 
 """
 Solve for ϵ
 
 See eq(31)
 """
-function schedule(ρ, v)
-    if ρ <= eps(ρ)
-        zero(ρ)
-    else
-        find_zero(ϵ -> ϵ^2 + v * ϵ^(3/2) - ρ^2, (0, ρ))
-    end
+function update_epsilon(u_posterior, v)
+    mean_u = mean(u_posterior)
+    ϵ_new = mean_u <= eps() ? zero(mean_u) : Roots.find_zero(ϵ -> ϵ^2 + v * ϵ^(3/2) - mean_u^2, (0, mean_u))
+    ϵ_new, mean_u
 end
 
-"""
-Compute mean of distances WHY???
-"""
-function rho_mean(ϵ, dists)
-    if ϵ <= eps()
-        zero(eps)
-    else
-        (sum(exp.(-dists ./ ϵ) .* dists)) ./  sum(exp.(-dists ./ ϵ))
-    end
-end
 
 """
 Resample ensample
 
 """
-function resample_ensample!(E, dist_E, U, δ)
-    n = length(E)
-    w = exp.(-dist_E .* δ ./ U)
+function resample_ensample!(samples_posterior, u_posterior, mean_u, δ)
+    n = length(samples_posterior)
+    w = exp.(-u_posterior .* δ ./ mean_u)
     idx_resampled = sample(1:n, weights(w), n, replace=true)
 
-    permute!(E, idx_resampled)
-    permute!(dist_E, idx_resampled)
+    permute!(samples_posterior, idx_resampled)
+    permute!(u_posterior, idx_resampled)
 
     @info "Resampling. Effective sample size: $(round(1/sum(abs2, w ./ sum(w)), digits=2))"
 end
@@ -56,14 +43,14 @@ end
 """
 Estimate the coavariance for the jump distributions from an ensemble
 """
-function estimate_jump_covariance(E, β)
-    β * cov(stack(E, dims=1)) + 1e-6*I
+function estimate_jump_covariance(samples_posterior, β)
+    β * cov(stack(samples_posterior, dims=1)) + 1e-6*I
 end
 
 """
-    Initialisation step
+# Initialisation step
 
-# Arguments
+## # Arguments
 See docs for `sabc`.
 """
 function initialization_noninf(f_dist, prior::Distribution, args...;
@@ -72,22 +59,18 @@ function initialization_noninf(f_dist, prior::Distribution, args...;
 
     ## ------------------------
     ## Initialize containers
-    ## ------------------------
 
     θ = rand(prior)
-    E = Vector{typeof(θ)}(undef, n_particles)
-    P = typeof(θ)[]
-
-    dist_E = Vector{Float64}(undef, n_particles)
-    dist_P = Float64[]
+    samples_posterior = Vector{typeof(θ)}(undef, n_particles)
+    dist_posterior = Vector{Float64}(undef, n_particles)
+    dist_prior = Float64[]
 
 
     ## ------------------
-    ## Initialization
-    ## ------------------
+    ## Build prior sample
 
     iter = 0
-    counter = 0 # Number of accepted particles in E
+    counter = 0 # Number of accepted particles in samples_posterior
     progbar = ProgressMeter.Progress(n_particles, desc="Generating initial ensemble...", dt=0.5)
     while counter < n_particles
 
@@ -100,38 +83,35 @@ function initialization_noninf(f_dist, prior::Distribution, args...;
         θ = rand(prior)
         ρ = f_dist(θ, args...; kwargs...)
 
-        ## store parameter in P
-        push!(P, θ)
-        push!(dist_P, ρ)
+        ## store distance
+        push!(dist_prior, ρ)
 
-        ## Accept with Prob = exp(-rho.p/eps.init) and store in E
+        ## Accept with Prob = exp(-rho.p/eps.init) and store in samples_posterior
         if rand() < exp(-ρ/eps_init)
             counter += 1
-            E[counter] = θ
-            dist_E[counter] = ρ
+            samples_posterior[counter] = θ
+            dist_posterior[counter] = ρ
             ProgressMeter.next!(progbar)
         end
     end
 
+    ## ----------------------------------
+    ## Compute ϵ
 
     ## empirical cdf of ρ under the prior
-    cdf_G = ecdf(dist_P)
+    cdf_G = ecdf(dist_prior)
 
-    dist_E = cdf_G(dist_E)
-    dist_P = cdf_G(dist_P)
+    u_posterior = cdf_G(dist_posterior)
 
-    ##
-    U = mean(dist_E)            # CHECK! This is different in R code!
-    ## or
-    U = rho_mean(eps_init, dist_E)
+    ϵ, mean_u = update_epsilon(u_posterior, v)
+    Σ_jump = estimate_jump_covariance(samples_posterior, β)
 
-    # γ = ... ????              # Missing in R code? Compare to paper!
+    n_simulation = length(dist_prior)
+    @info "Ensample with $n_particles particles initialised with $n_simulation simulation."
 
-    ϵ = schedule(U, v)
-
-    Σ_jump = estimate_jump_covariance(E, β)
-
-    return (E=E, dist_E=dist_E, P=P, U=U, ϵ=ϵ, cdf_G=cdf_G, Σ_jump=Σ_jump)
+    return (samples_posterior=samples_posterior, u_posterior=u_posterior,
+            mean_u=mean_u, ϵ=ϵ, cdf_G=cdf_G, Σ_jump=Σ_jump,
+            n_simulation_init = n_simulation)
 
 end
 
@@ -139,8 +119,8 @@ end
 
 """
 ```
-    sabc(f_dist,prior::Distribition, args...;
-                     n_particles = 100, n_simulation=10_000,
+    sabc(f_dist, prior::Distribition, args...;
+                     n_particles = 100, n_simulation = 10_000,
                      eps_init = 1.0,
                      resample = n_particles,
                      v=0.3, β=1.0, δ=0.9,
@@ -148,9 +128,9 @@ end
                      kwargs...)
 
 ```
-# Simulated Annealing Approximtaive Bayeisain Inference Algorithm
+# Simulated Annealing Approximtaive Bayesian Inference Algorithm
 
-# Arguments
+## Arguments
 - `f_dist`: Function that distance between data and a random sample from the likelihood. The first argument must be the parameter vector.
 - `prior`: A `Distribution` defining the prior.
 - `args...`: Further arguments passed to `f_dist`
@@ -163,16 +143,15 @@ end
 - `resample`: After how many accepted updates?
 - `kwargs...`: Further arguments passed to `f_dist``
 
-# Returns
+## Returns
 - A named tuple with the following keys:
     - `posterior_samples`
-    - `prior_samples`
     - `eps`: value of epsilon at the last iteration.
 """
 function sabc(f_dist, prior::Distribution, args...;
               n_particles = 100, n_simulation = 10_000,
               eps_init,
-              resample = n_particles, # CHECK! meaningful default?
+              resample = n_particles,
               v=0.3, β=1.5, δ=0.9,
               adaptjump = true,
               kwargs...)
@@ -182,74 +161,75 @@ function sabc(f_dist, prior::Distribution, args...;
     ## Initialize
     ## ------------------------
 
-    @unpack E, dist_E, U, ϵ, Σ_jump, cdf_G, P = initialization_noninf(f_dist, prior, args...;
-                                                                      n_particles = n_particles,
-                                                                      n_simulation = n_simulation,
-                                                                      eps_init = eps_init,
-                                                                      v=v, β=β,
-                                                                      kwargs...)
+    @unpack (samples_posterior, u_posterior, mean_u, ϵ, Σ_jump, cdf_G, n_simulation_init) =
+        initialization_noninf(f_dist, prior, args...;
+                              n_particles = n_particles,
+                              n_simulation = n_simulation,
+                              eps_init = eps_init,
+                              v=v, β=β,
+                              kwargs...)
 
     dim_par = size(prior)
-    n_simulation_init = length(P)
-    @info "Ensample with $n_particles particles initialised with $n_simulation_init simulation."
+
 
     ## --------------
     ## Sampling
     ## --------------
 
     progbar = ProgressMeter.Progress(n_simulation, desc="Sampling...", dt=0.5)
-    show_summary(ϵ, U) = () -> [(:eps, ϵ), (:mean_distance, U)]
+    show_summary(ϵ, mean_u) = () -> [(:eps, ϵ), (:mean_transformed_distance, mean_u)]
 
-    accept = 0
+    n_accept = 0
     for _ in 1:(n_simulation - n_simulation_init)
 
-        idx = rand(1:n_particles)  # pick a particle
+        ## -- update all particles (this can be multithreaded)
+        for i in 1:n_particles
 
-        # proposal
-        θproposal = E[idx] .+ rand(MvNormal(zeros(dim_par), Σ_jump))
-        if pdf(prior, θproposal) > 0
-            dist_proposal = cdf_G(f_dist(θproposal, args...; kwargs...))
-            accept_prob = pdf(prior, θproposal) / pdf(prior, E[idx]) * exp((dist_E[idx] - dist_proposal) / ϵ)
-        else
-            accept_prob = 0.0
+            # proposal
+            θproposal = samples_posterior[i] .+ rand(MvNormal(zeros(dim_par), Σ_jump))
+
+            # acceptance probability
+            if pdf(prior, θproposal) > 0
+                u_proposal = cdf_G(f_dist(θproposal, args...; kwargs...))
+                accept_prob = pdf(prior, θproposal) / pdf(prior, samples_posterior[i]) * exp((u_posterior[i] - u_proposal) / ϵ)
+            else
+                accept_prob = 0.0
+            end
+
+            # if isnan(accept_prob)
+            #     accept_prob = u_posterior[i] < u_proposal ? 0.0 : 1.0
+            # end
+
+            if rand() < accept_prob
+                samples_posterior[i] = θproposal
+                u_posterior[i] = u_proposal
+                n_accept += 1
+            end
+
         end
 
-        # if isnan(accept_prob)
-        #     accept_prob = ifelse(dist_E[idx] < dist_proposal, 0.0, 1.0)
-        # end
-
-        if rand() < accept_prob
-            E[idx] = θproposal
-            dist_E[idx] = dist_proposal
-
-            if adaptjump
-                Σ_jump = estimate_jump_covariance(E, β) # Do we want to do this every time?
-             end
-
-             U = mean(dist_E)
-             ϵ = schedule(U, v)
-             accept += 1
+        ## -- update epsilon and jump distribution
+        if adaptjump
+            Σ_jump = estimate_jump_covariance(samples_posterior, β)
         end
 
+        ϵ, mean_u = update_epsilon(u_posterior, v)
 
-        # resample
-        if (accept >= resample) && (U > eps())
+        ## -- resample
+        if (n_accept >= resample) && (mean_u > eps())
 
-            resample_ensample!(E, dist_E, U, δ)
+            resample_ensample!(samples_posterior, u_posterior, mean_u, δ)
 
+            ϵ, mean_u = update_epsilon(u_posterior, v)
 
-            ϵ = ϵ * (1 - δ)
-            U = mean(dist_E)            # WHY?
-            ϵ = schedule(U, v)          # WHY? should be no change!
-
-            accept = 0
+            n_accept = 0
         end
 
         # update progressbar
-        ProgressMeter.next!(progbar, showvalues = show_summary(ϵ, U))
+        ProgressMeter.next!(progbar, showvalues = show_summary(ϵ, mean_u))
     end
 
-    return (posterior_samples=E, prior_samples=P, eps=ϵ)
+    return (posterior_samples=samples_posterior, eps=ϵ)
 end
 
 
