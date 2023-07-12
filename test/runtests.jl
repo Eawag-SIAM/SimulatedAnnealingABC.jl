53cd00fed4e77f5346893613cf3b83a9f733cb53
Andreas Scheidegger <andreas.scheidegger@eawag.ch>
2023-07-12 10:13:34 +0200

    add 1-dim case to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,40 @@ using Test
 using Distributions
 
 
-@testset "User Interface" begin
+@testset "Sampling 1-dim" begin
+
+    ## Define model
+    f_dist(θ) = 0 - rand(Normal(θ[1], 1))
+    prior = Uniform(-10,10)
+
+    # n_simulation too small
+    @test_throws ErrorException sabc(f_dist, prior; eps_init = 1,
+                                     n_particles = 100, n_simulation = 10)
+
+
+    res = sabc(f_dist, prior; eps_init = 10,
+               n_particles = 100, n_simulation = 1000)
+
+    @test res.state.n_simulation <= 1000
+    @test length(res.population) == 100
+
+    ## update existing population
+    update_population!(res, f_dist, prior;
+                       n_simulation = 1_000)
+
+    @test res.state.n_simulation <= 1000 + 1000
+
+    ## update existing population with too few simulation, i.e.
+    ##  n_simulation < n_particles
+    n_sim = res.state.n_simulation
+    update_population!(res, f_dist, prior;
+                       n_simulation = 50)
+    @test res.state.n_simulation == n_sim # no updating
+
+end
+
+
+@testset "Sampling n-dim" begin
 
     ## Define model
     f_dist(θ) = sum(abs2, rand(Normal(θ[1], θ[2]), 4))
