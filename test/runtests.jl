e25e0e65b32a66060ad156b0f99a246426055366
Andreas Scheidegger <andreas.scheidegger@eawag.ch>
2023-07-20 17:00:14 +0200

    add (very trivial) test for multiple summary statistics

--- test/runtests.jl
+++ test/runtests.jl
@@ -77,8 +77,28 @@ end
     @testset "Sampling 1-dim" begin
         # TOOO
     end
+
     @testset "Sampling n-dim" begin
-        # TOOO
+
+        # define prior
+        prior = product_distribution([Normal(0,1),   # theta[1]
+                                      Uniform(0,2)]) # theta[2]
+
+        # compte two summary statistics
+        function f_dist(θ, y)
+            y_samp = rand(Normal(θ[1], θ[2]), 10)
+            (abs(0 - sum(y_samp .- 0)), abs(sum(1 .- (y_samp .- 0).^2)))
+        end
+
+
+        y_obs = randn(10)
+
+        ## Sample Posterior
+        res = sabc(f_dist, prior, y_obs;
+                   n_particles = 1000, n_simulation = 10_000);
+
+        @test res.state.ϵ < 1
+
     end
 end
 
