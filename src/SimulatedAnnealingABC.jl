54ea430cf1e7e7cf9df29fffe6876183b0432cbc
Simone Ulzega <ulzg@login02.cm.cluster>
2024-03-01 10:26:01 +0100

    testing threads number > 32

--- src/SimulatedAnnealingABC.jl
+++ src/SimulatedAnnealingABC.jl
@@ -366,7 +366,7 @@ function update_population!(population_state::SABCresult, f_dist, prior, args...
                 rpopulation = Ref(population)
                 ru = Ref(u)
                 rρ = Ref(ρ)
-                @floop ThreadedEx(basesize = 3*Threads.nthreads()) for i in eachindex(population)
+                @floop ThreadedEx(basesize = 10*Threads.nthreads()) for i in eachindex(population)
                     # proposal
                     @inbounds θproposal = proposal(rpopulation[][i], Σ_jump)
 
